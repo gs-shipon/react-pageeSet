b679973c56d4b5418247d896afd7cc5b0dbd0515
GS-Shipon <shiponxi990@gmail.com>
2026-03-03 02:16:05 +0600

    add

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div  className='text-6xl text-blue-600 hover:'>this is abour page</div>
+    <div  className='text-6xl text-blue-500 hover:'>this is abour page</div>
   )
 }
 
